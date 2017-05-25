a0ed5b7fb3c9a7a804aa921c56fcc618608dbc5f
AWESOMEMELOVER <nikitakyrpenko@gmail.com>
2017-05-25 18:12:11 +0300

    Practice2

--- Assets/HeroRabit.cs
+++ Assets/HeroRabit.cs
@@ -6,15 +6,65 @@ public class HeroRabit : MonoBehaviour {
 
 	public float speed = 20;
 	Rigidbody2D myBody = null;
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
 		myBody = this.GetComponent<Rigidbody2D> ();
+		LevelController.current.setStartPosition (transform.position);
+	}
+
+	void FixedUpdate(){
+		float diff = Time.deltaTime;
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+		Debug.DrawLine (from, to, Color.red);
+
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			this.JumpActive = true;
+		}
+		if(this.JumpActive) {
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+		Animator animator = GetComponent<Animator> ();
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		float value = Input.GetAxis ("Horizontal");
+		Animator animator = GetComponent<Animator> ();
+		if(Mathf.Abs(value) > 0) {
+			animator.SetBool ("run", true);
+		} else {
+			animator.SetBool ("run", false);
+		}
 		if (Mathf.Abs (value) > 0) {
 			Vector2 vel = myBody.velocity;
 			vel.x = value * speed;
---
+++ Assets/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+	Vector3 startingPosition;
+
+	public void setStartPosition(Vector3 pos) {
+		this.startingPosition = pos;
+	}
+
+	public void onRabitDeath(HeroRabit rabit) {
+		rabit.transform.position = this.startingPosition;
+	}
+
+	public static LevelController current;
+	void Awake(){
+		current = this;
+	}
+
+}
